7d7f71d6858b16ab43229a8aeeaaadd84c35af39
David Boyer <odavzo@vps553835.ovh.net>
2020-06-10 00:10:47 +0200

    Clean repo

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-killall useless_bot
-git pull -f https://github.com/odavzo/Useless-Bot.git
-go build -o ./useless_bot ./main.go
-./useless_bot -t NzE3MzI4ODMxMjYxNzA0MjUz.XtjreQ.mLUQDcrXUmtnx6wBBoNG4gWvKx4 2> ./log/error.log > /log/out.log &
-
